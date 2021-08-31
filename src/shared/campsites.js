78dd10044f8bda8f6e7ae1db5a086082ecd9eb98
Billy <87097747+BillyHacker@users.noreply.github.com>
2021-08-31 12:23:54 -0700

    React Component Types

--- src/shared/campsites.js
+++ src/shared/campsites.js
@@ -4,7 +4,15 @@ export const CAMPSITES = [
         name: 'React Lake Campground',
         image: 'assets/react-lake.jpg',
         elevation: 1233,
-        description: "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers."
+        description: "Nestled in the foothills of the Chrome Mountains, this campground on the shores of the pristine React Lake is a favorite for fly fishers.",
+
+        comment: {
+            text: "What a magnifivant View",
+            author: 'Tinus Lorvaldes',
+            date:"Oct 25, 2018",
+        }
+       
+       
     },
     {
         id: 1,
@@ -12,6 +20,7 @@ export const CAMPSITES = [
         image: 'assets/chrome-river.jpg',
         elevation: 877,
         description: "Spend a few sunny days and starry nights beneath a canopy of old-growth firs at this enchanting spot by the Chrome River."
+        
     },
     {
         id: 2,
@@ -25,6 +34,15 @@ export const CAMPSITES = [
         name: 'Redux Woods Campground',
         image: 'assets/redux-woods.jpg',
         elevation: 42,
-        description: "You'll never want to leave this hidden gem, deep within the lush Redux Woods."
+        description: "You'll never want to leave this hidden gem, deep within the lush Redux Woods.",
+
+
+
+
     }
+
+
+    
+    
+    
 ];
